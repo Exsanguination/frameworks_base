d1a9f793954d8185d7ea5c3ffa3088c6626e49ac
BigBrother1984 <carlosavignano@aospa.co>
2014-11-18 14:34:35 +0100

    SystemUI: Materialize volume panel in/out animation
    * Thanks to @David96 that had the original idea, material
      oriented, already during kitkat times.
    
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: Ic79d404c4a0d5bc0a83d20cd4e0d7d5ad7b7c90f
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -109,6 +109,7 @@ public class VolumePanel extends Handler implements DemoMode {
     private static final int TIMEOUT_DELAY_COLLAPSED = 4500;
     private static final int TIMEOUT_DELAY_SAFETY_WARNING = 5000;
     private static final int TIMEOUT_DELAY_EXPANDED = 10000;
+    private static final int ANIMATION_DURATION = 350; // ms
 
     private static final int MSG_VOLUME_CHANGED = 0;
     private static final int MSG_FREE_RESOURCES = 1;
@@ -1301,11 +1302,30 @@ public class VolumePanel extends Handler implements DemoMode {
             if (stream != STREAM_MASTER) {
                 mAudioManager.forceVolumeControlStream(stream);
             }
-            mDialog.show();
-            if (mCallback != null) {
-                mCallback.onVisible(true);
+             if (mDialog != null) {
+                mDialog.show();
+                Runnable r = new Runnable() {
+                    public void run() {
+                        mView.setY(-mView.getHeight());
+                        mView.animate().y(0).setDuration(ANIMATION_DURATION)
+                            .withEndAction(new Runnable() {
+                                public void run() {
+                                    if (mCallback != null) {
+                                        mCallback.onVisible(true);
+                                    }
+                                    announceDialogShown();
+                                }
+                            });
+                    }
+                };
+                if (mView.getHeight() == 0) {
+                    new Handler().post(r);
+                } else {
+                    r.run();
+                }
+            } else {
+                Log.d(mTag, "Bad, mDialog is null...");
             }
-            announceDialogShown();
         }
 
         // Do a little vibrate if applicable (only when going into vibrate mode)
@@ -1559,7 +1579,6 @@ public class VolumePanel extends Handler implements DemoMode {
 
             case MSG_TIMEOUT: {
                 if (isShowing()) {
-                    hideVolumePanel();
                     if (mDialog != null) {
                         mView.animate().y(-mView.getHeight())
                                 .setDuration(ANIMATION_DURATION)
